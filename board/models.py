724cb43160f9779eb1a46fd387b9c7315953544a
Alex <alexkravchenko116@gmail.com>
2021-12-29 19:13:15 +0200

    created some models and wrote a little logic

--- board/models.py
+++ board/models.py
@@ -2,13 +2,31 @@ from django.db import models
 
 
 class Bd(models.Model):
+    rubric = models.ForeignKey(
+        'Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика'
+    )
     title = models.CharField(max_length=50, verbose_name='Товар')
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
-    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публикации')
-    
-    
+    published = models.DateTimeField(
+        auto_now_add=True, db_index=True, verbose_name='Дата публикации'
+    )
+
     class Meta:
         verbose_name_plural = 'Объявления'
         verbose_name = 'Объявлениe'
         ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField(
+        max_length=50, verbose_name='Название', db_index=True
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
--- board/views.py
+++ board/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django .template import loader 
-
-from .models import Bd
+from .models import Bd, Rubric
 
 
-def info (request):
+def info(request):
     bbs = Bd.objects.all()
-    return render(request, 'board/index.html', {'bbs': bbs}) 
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'board/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bd.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {
+        'bbs': bbs, 'rubrics': rubrics,
+        'current_rubric': current_rubric
+    }
+    return render(request, 'board/by_rubric.html', context)
